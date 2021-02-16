22fd0eca1cbb8b10cd84ed891e96ec91566124d4
Jason Prince <jasoncprince@gmail.com>
2021-02-16 15:35:59 -0700

    added hero and illustration components

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -1,11 +1,15 @@
 import styles from "./styles.module.css";
+import Illustration from "./Illustration";
 
 const Hero = () => {
   return (
     <section className="styles.hero">
       <div className="styles.hero-content">
         <h1>Griffin EnerG Consulting</h1>
+        <p>Know your options</p>
+        <button className="styles.contact-me">Contact Me</button>
       </div>
+      <Illustration />
     </section>
   );
 };
---
+++ components/hero/Illustration.jsx
@@ -0,0 +1,7 @@
+import styles from "./styles.module.css";
+
+const Illustration = () => {
+  return <div className="styles.illustration">illustration here</div>;
+};
+
+export default Illustration;
